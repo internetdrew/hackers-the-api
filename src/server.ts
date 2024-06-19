56352b8fbdc13190c8995d7337a362b741956e30
Andrew Rowley <andrew@internetdrew.com>
2024-06-19 14:12:25 -0400

    add helmet

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import compression from 'compression';
+import helmet from 'helmet';
 
 import { adminRouter, v1Router } from './routes/v1';
 import { isAdmin, protect } from './modules/auth';
@@ -15,6 +16,7 @@ import {
 const app = express();
 
 app.use(compression());
+app.use(helmet());
 app.use(express.static('public'));
 app.use(cors());
 app.use(morgan('dev'));
