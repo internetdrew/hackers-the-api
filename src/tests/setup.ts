efa3e7aac915f38fd6ee493c117d8b2a604fb022
Andrew Rowley <andrew@internetdrew.com>
2024-06-26 09:13:44 -0400

    clear organizations table before each test

---
+++ src/tests/setup.ts
@@ -0,0 +1,10 @@
+import prisma from '../db';
+import { beforeEach } from 'vitest';
+
+beforeEach(async () => {
+  await prisma.$transaction([
+    prisma.user.deleteMany(),
+    prisma.character.deleteMany(),
+    prisma.organization.deleteMany(),
+  ]);
+});
