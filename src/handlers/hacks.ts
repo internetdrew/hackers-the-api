1da38a3556e4dd2f7a6bb5c5c5fd845f697cb79a
Andrew Rowley <andrew@internetdrew.com>
2024-06-26 15:11:27 -0400

    expand creation code to battle duplicate creation

--- src/handlers/hacks.ts
+++ src/handlers/hacks.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import prisma from '../db';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 export const getAllHacks = async (req: Request, res: Response) => {
   const hacks = await prisma.hack.findMany({
@@ -54,15 +55,23 @@ export const getHackHackers = async (req: Request, res: Response) => {
   return res.json({ data: hack?.associatedHackers });
 };
 
-export const getHackerHacks = async (req: Request, res: Response) => {
-  const hacker = await prisma.character.findUnique({
-    where: {
-      id: parseInt(req.params?.id),
-    },
-    include: {
-      hacksAsHacker: true,
-    },
-  });
-
-  return res.json({ data: hacker?.hacksAsHacker });
+export const createHack = async (req: Request, res: Response) => {
+  try {
+    const hack = await prisma.hack.create({
+      data: req.body,
+    });
+    res.json({ data: hack });
+  } catch (error) {
+    if (
+      error instanceof PrismaClientKnownRequestError &&
+      error.code === 'P2002'
+    ) {
+      return res.status(409).json({
+        message: 'This hack already exists.',
+      });
+    }
+    res.status(500).json({
+      error: 'An unexpected error has occurred while creating a hack.',
+    });
+  }
 };
