8d233fbe3482ed752b62236c962bb2e5ae7e0acc
Andrew Rowley <andrew@internetdrew.com>
2024-06-27 16:06:43 -0400

    seperate routers

---
+++ src/routers/admin.ts
@@ -0,0 +1,75 @@
+import { Router } from 'express';
+import {
+  handleInputErrors,
+  validateCharacterCreationInputs,
+  validateHackContributorInput,
+  validateHackCreationInputs,
+  validateOrganizationCreationInputs,
+} from '../modules/middleware';
+import {
+  createCharacter,
+  deleteCharacter,
+  updateCharacter,
+} from '../handlers/characters';
+import {
+  createOrganization,
+  deleteOrganization,
+  updateOrganization,
+} from '../handlers/organizations';
+import { createHack, addHackContributor, updateHack } from '../handlers/hacks';
+import { authorizeAdmin } from '../handlers/user';
+
+const adminRouter = Router();
+
+/*
+Characters
+*/
+adminRouter.post(
+  '/characters',
+  validateCharacterCreationInputs,
+  handleInputErrors,
+  createCharacter
+);
+adminRouter.patch('/characters/:id', updateCharacter);
+adminRouter.delete('/characters/:id', deleteCharacter);
+
+/*
+Organizations
+*/
+adminRouter.post(
+  '/organizations',
+  validateOrganizationCreationInputs,
+  handleInputErrors,
+  createOrganization
+);
+adminRouter.patch('/organizations/:id', updateOrganization);
+adminRouter.delete('/organizations/:id', deleteOrganization);
+
+/* 
+Hacks
+*/
+adminRouter.post(
+  '/hacks',
+  validateHackCreationInputs,
+  handleInputErrors,
+  createHack
+);
+adminRouter.patch('/hacks/:id', updateHack);
+adminRouter.post(
+  '/hacks/:id/contribution',
+  validateHackContributorInput,
+  handleInputErrors,
+  addHackContributor
+);
+// adminRouter.post(
+//   '/hacks/:id/hackers',
+//   validateHackContributorCreationInputs,
+//   handleInputErrors,
+//   createHackContributor
+// );
+// adminRouter.delete('/hacks/delete/:id', deleteHack);
+
+/* Admin (Users) */
+adminRouter.patch('/authorize', handleInputErrors, authorizeAdmin);
+
+export default adminRouter;
