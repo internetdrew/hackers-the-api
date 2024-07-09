8f1de6616e2ae56c71f7f96731fe5929fb2823fd
Andrew Rowley <andrew@internetdrew.com>
2024-07-09 15:15:02 -0400

    update to 3000 when no env port is present

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import { startMetricsServer } from './modules/metrics';
 import app from './server';
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app?.listen(port, () => {
   console.log(`Server is running on port ${port}`);
