3b95cf93bbb9818dc51bd80b48e06759d82ce756
Andrew Rowley <andrew@internetdrew.com>
2024-07-22 16:13:06 -0400

    add prod contingency

--- client/src/components/Welcome.tsx
+++ client/src/components/Welcome.tsx
@@ -6,6 +6,12 @@ import Forms from "./Forms";
 import "../index.css";
 
 const Welcome = () => {
+  const baseUrl = import.meta.env.PROD
+    ? "https://hackerstheapi.com"
+    : "http://localhost:3000";
+
+  console.log("production env?: ", import.meta.env.PROD);
+
   const fetcher = async (url: string) =>
     axios
       .get(url, {
@@ -13,10 +19,7 @@ const Welcome = () => {
       })
       .then((res) => res.data);
 
-  const { data, isLoading } = useSWR(
-    `${import.meta.env.PUBLIC_API_URL}/auth/check-auth`,
-    fetcher,
-  );
+  const { data, isLoading } = useSWR(`${baseUrl}/auth/check-auth`, fetcher);
 
   if (isLoading) {
     return (
