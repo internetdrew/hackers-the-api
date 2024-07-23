52fd96f6a8310af1d2eba7ce25a25cd96451c439
Andrew Rowley <andrew@internetdrew.com>
2024-07-23 15:57:47 -0400

    pull down containers

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,8 @@
 echo "Pulling latest..."
 git pull
 
+echo "Breaking down containers..."
+docker compose down
+
 echo "Building the app..."
 PUBLIC_API_URL=https://hackerstheapi.com docker compose up -d --build
